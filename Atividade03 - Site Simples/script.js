9960c70e415e1fbe3a27b0106b1539bb0df23942
Raquel Maciel <raquel.maciel.coelho09@aluno.ifce.edu.br>
2021-09-01 21:41:00 -0300

    chore: aperfeiçoamento do site

--- Atividade03 - Site Simples/script.js
+++ Atividade03 - Site Simples/script.js
@@ -3,22 +3,3 @@ function playMyTale(taleName) {
     console.log(taleName)
     audio.play()
 }
-// // JavaScript demonstration
-// function doDemo (taleName) {
-//     var square = document.getElementById("square");
-//     // square.style.backgroundColor = "#fa4";
-//     // button.setAttribute("disabled", "true");
-    
-//     square.play()
-//     //setTimeout(clearDemo, 1500, button);
-// }
-
-// function clearDemo (taleName) {
-//     var square = document.getElementById("square");
-//     // square.style.backgroundColor = "transparent";
-//     // button.removeAttribute("disabled");
-//     square.pause()
-// }
-
-// var audio = document.getElementById("square");
-// $("#square").mouseenter(function() { audio.play();});
