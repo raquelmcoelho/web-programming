017fed279830a87e63c1e5e5f36baea1e6ddd0ee
Raquel Maciel <raquel.maciel.coelho09@aluno.ifce.edu.br>
2021-12-06 21:03:04 -0300

    chore: adaptações

--- Avaliação Final/script.js
+++ Avaliação Final/script.js
@@ -1,13 +1,24 @@
-function validaForm(){
-    var radios = document.getElementsByName("pergunta1");
-    var formValido = false;
+var img = ["assets/checkbox-marked-v.svg", "assets/checkbox-marked-x.svg", "assets/checkbox-not-marked.svg"]
+document.getElementById("1A").addEventListener("click", function() {
+    console.log(document.getElementById("1A").src);
+});
 
-    var i = 0;
-    while (!formValido && i < radios.length) {
-        if(radios[i].checked) formValido = true;
-        i++;        
+function checkQuestion(number, option){
+    switch(number){
+        case 1:
+            // Checar se nem 2,3,4,5 questão tem o item escolhido
+            break;
+        case 2:
+            // É a única questão com a mesma resposta que esta questão
+            break;
+        case 3:
+            // É a resposta da questão número 5
+            break;
+        case 4:
+            // É a primeira questão cuja resposta é a letra A
+            break;
+        case 5:
+            // É a resposta da questão número 3
+            break;
     }
-
-    if (!formValido) alert("Por favor, escolha uma opção");
-    return formValido;
 }
